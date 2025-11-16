e53cc2d7012efa00cef66c5b41c9ad4c285c12da
Alen <alen20780@gmail.com>
2025-11-16 18:58:48 +0530

    variable example

---
+++ variable.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo "There once was a man named Alen <br>";
+        echo "He was 21 years old <br>";
+        echo "He really liked the name Alen <br>";
+        echo "But didn't like being 21 <br>";
+        echo "<hr>";
+        $name = "Alen";
+        $age = 21;
+        echo "There once was a man named $name <br>";
+        echo "He was $age years old <br>";
+        $name = "Sarungbam Alen Meetei";
+        echo "He really liked the name $name <br>";
+        echo "But didn't like being $age <br>";
+    ?>
+</body>
+</html>
